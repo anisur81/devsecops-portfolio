0d6ecbc55e18589c7c1eea168ecf673c2216a3ff
anisur81 <anisur81@gmail.com>
2026-04-30 14:30:19 +0600

    Updated project 24

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'skills',
     'projects',
     'about',
-    'tests',
 ]
 
 MIDDLEWARE = [
--- tests/test_urls.py
+++
@@ -1,9 +0,0 @@
-from django.tests import SimpleTestCase
-from django.urls import reverse, resolve
-from tests.views import home   # ✅ FIXED
-
-class TestUrls(SimpleTestCase):
-
-    def test_home_url_resolves(self):
-        url = reverse("home")
-        self.assertEqual(resolve(url).func, home)
